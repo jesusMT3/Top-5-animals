b6f0a8f20bba7918573cf467f4e2fcff640fc062
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-09-05 13:11:20 +0200

    create - plot_gender, plot_country, plot_age

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ from matplotlib import colormaps
 
 def plot_ranking(ranking: pd.DataFrame):
     """
-    Draws a bar plot of the ranking database.
+    Draws a bar plot of the ranking dataframe.
 
     Args:
         ranking (pd.DataFrame)
@@ -17,7 +17,40 @@ def plot_ranking(ranking: pd.DataFrame):
     all_colors = [gold, silver, bronze] + [rest] * (len(ranking) - 3)
 
     fig, ax = plt.subplots(figsize=(12, 9))
-    ax.bar(ranking.index, ranking["values"], color=all_colors)
     plt.xticks(rotation=45)
+    ax2 = ax.twinx()
+    ax.bar(ranking.index, ranking["values"], color=all_colors)
+    ax2.plot(ranking.index, ranking["votes"], 'or')
     
     plt.show()
+
+
+def plot_gender(gender_distribution: pd.DataFrame):
+    """
+    Draws a bar plot of the gender distribution in the data.
+
+    Args:
+        ranking (pd.DataFrame)
+    """
+    
+    raise NotImplementedError
+
+
+def plot_country(country_distribution: pd.DataFrame):
+    """
+    Draws a bar plot of the country distribution in the data.
+
+    Args:
+        ranking (pd.DataFrame)
+    """
+    raise NotImplementedError
+
+
+def plot_age(age_distribution: pd.DataFrame):
+    """
+    Draws a bar plot of the rage distribution in the data.
+
+    Args:
+        ranking (pd.DataFrame)
+    """
+    raise NotImplementedError
