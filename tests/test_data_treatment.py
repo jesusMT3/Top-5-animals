3bbcd86cc6f18a50a461ced3daf95785a0053cf6
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-09-12 10:41:29 +0200

    test - data filters

--- tests/test_data_treatment.py
+++ tests/test_data_treatment.py
@@ -8,6 +8,9 @@ SHEET_ID = '1KCmNGGoHiHm16pI6D14MY_GUdizSpOdJfnQNLMQLDFw'
 url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
 # Unit tests for import_data()
+DATA_ROWS = 6
+DATA_COLS = 10
+
 def test_import_data_imports():
     data = import_data(url)
     assert data is not None
@@ -16,20 +19,15 @@ def test_import_data_type():
     data = import_data(url)
     assert isinstance(data, pd.DataFrame)
 
-def test_import_data_row_count():
+def test_import_data_size():
     data = import_data(url)
-    assert len(data) == 6
-
-def test_import_data_column_count():
-    data = import_data(url)
-    assert len(data.columns) == 10
-
-def test_import_data_content():
-    data = import_data(url)
-    assert data.loc[2, "Age"] == 23
-    assert data.loc[3, "Country"] == "Norway"
+    assert len(data) == DATA_ROWS
+    assert len(data.columns) == DATA_COLS
 
 # Unit tests for make_ranking()
+RANKING_ROWS = 10
+RANKING_COLS = 2
+
 def test_make_ranking_imports():
     data = import_data(url)
     ranking = make_ranking(data)
@@ -39,3 +37,14 @@ def test_make_ranking_type():
     data = import_data(url)
     ranking = make_ranking(data)
     assert isinstance(ranking, pd.DataFrame)
+
+def test_make_ranking_size():
+    data = import_data(url)
+    ranking = make_ranking(data)
+    assert len(ranking) == RANKING_ROWS
+    assert len(ranking.columns) == RANKING_COLS
+
+def test_make_ranking_filter():
+    data = import_data(url)
+    ranking = make_ranking(data, custom_filter = ("Gender", "Male"))
+    assert len(ranking) == RANKING_ROWS / 2
