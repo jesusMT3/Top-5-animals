7a13d3b471fa81413d0fffa9df765e7298f05590
Jesus Marcos Torero <jesusmarcostorero@gmail.com>
2024-09-12 09:59:42 +0200

    test - data_treatment()

--- tests/test_data_treatment.py
+++ tests/test_data_treatment.py
@@ -1,33 +1,41 @@
-from data_treatment import import_data, make_ranking
+# pylint: skip-file
+
 import pandas as pd
+from data_treatment import import_data, make_ranking
 
-sheet_name = 'test'
-sheet_id = '1KCmNGGoHiHm16pI6D14MY_GUdizSpOdJfnQNLMQLDFw'
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+SHEET_NAME = 'test'
+SHEET_ID = '1KCmNGGoHiHm16pI6D14MY_GUdizSpOdJfnQNLMQLDFw'
+url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
-def test_import_data():
+# Unit tests for import_data()
+def test_import_data_imports():
     data = import_data(url)
-    # import_data imports
     assert data is not None
-    
-    # import data returns a dataframe
-    assert type(data) == pd.DataFrame
-    
-def test_contents_import_data():
-    data = import_data(url)
-    # import data returns the desired rows
-    assert len(data) == 6
-    
-    # import data returns the desired columns
-    assert len(data.columns) == 10
-    
 
-def test_make_ranking():
+def test_import_data_type():
+    data = import_data(url)
+    assert isinstance(data, pd.DataFrame)
+
+def test_import_data_row_count():
+    data = import_data(url)
+    assert len(data) == 6
+
+def test_import_data_column_count():
+    data = import_data(url)
+    assert len(data.columns) == 10
+
+def test_import_data_content():
+    data = import_data(url)
+    assert data.loc[2, "Age"] == 23
+    assert data.loc[3, "Country"] == "Norway"
+
+# Unit tests for make_ranking()
+def test_make_ranking_imports():
     data = import_data(url)
     ranking = make_ranking(data)
-    
-    # ranking returns 
     assert ranking is not None
-    
-    # ranking is a dataframe
-    assert type(ranking) == pd.DataFrame
+
+def test_make_ranking_type():
+    data = import_data(url)
+    ranking = make_ranking(data)
+    assert isinstance(ranking, pd.DataFrame)
